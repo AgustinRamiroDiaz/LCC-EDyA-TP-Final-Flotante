3a153805fb1f62391660bf8c95a7ad48343df886
AgustinRamiroDiaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2019-08-24 02:49:50 -0300

    Update held_karp.h

--- held_karp.h
+++ held_karp.h
@@ -1,11 +1,9 @@
 #ifndef HELDKARP
 #define HELDKARP
 
-//llena la matrizHeldKarp
 //la columna representa el vertice desde el cual quiero llegar
 //la fila n representa el subconjunto del grafo a recorrer en binario
 //el primer nodo (el cual se omite en la matriz) es al cual quiero llegar
-void LlenarMatrizHeldKarp(int** matrizHeldKarp, int** matrizAdyacente, int* caminoVertices, int n);
 
 //calcula el valor de la matrizHeldKarp en la fila y columna de manera recursiva
 //almacenando el vertice por el cual se pasa en caminoVertices
